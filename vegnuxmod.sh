cdac5c9589d97c1d5aa93c3337c3c65d09a1e026
cargabsj175 <cargabsj175@gmail.com>
2014-12-26 23:15:26 -0430

    added euskara language.

--- vegnuxmod.sh
+++ vegnuxmod.sh
@@ -121,6 +121,7 @@ cat << EOF > .repo/local_manifests/extra.xml
 <project path="gaia-l10n/el" name="l10n/el/gaia.git" remote="mozillaorg" revision="master" />
 <project path="gaia-l10n/eo" name="l10n/eo/gaia.git" remote="mozillaorg" revision="master" />
 <project path="gaia-l10n/es" name="l10n/es/gaia.git" remote="mozillaorg" revision="master" />
+<project path="gaia-l10n/eu" name="l10n/eu/gaia.git" remote="mozillaorg" revision="master" />
 <project path="gaia-l10n/fr" name="l10n/fr/gaia.git" remote="mozillaorg" revision="master" />
 <project path="gaia-l10n/hu" name="l10n/hu/gaia.git" remote="mozillaorg" revision="master" />
 <project path="gaia-l10n/it" name="l10n/it/gaia.git" remote="mozillaorg" revision="master" />
@@ -146,6 +147,7 @@ cat << EOF > gaia-l10n/languages_dev.json
 "en-US" : "English (US)",
 "el" : "Ελληνικά",
 "eo" : "Esperanto",
+"eu" : "Euskara",
 "es" : "Español",
 "fr" : "Français",
 "hu" : "Magyar",
@@ -196,6 +198,7 @@ echo "** Actualizando los lenguajes de gaia..."
 ./repo sync gaia-l10n/el
 ./repo sync gaia-l10n/eo
 ./repo sync gaia-l10n/es
+./repo sync gaia-l10n/eu
 ./repo sync gaia-l10n/fr
 ./repo sync gaia-l10n/hu
 ./repo sync gaia-l10n/it
